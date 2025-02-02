cfa6ab3756d305505691da3b14c029a42403608f
Praveen Baruri <praveenb@snovasys.com>
2025-02-02 13:06:32 +0000

    counter component added

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './Components/Counter'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>Counter app</h1>
+        <h1>Counter</h1>
+        <Counter />
       </div>
     </>
   )
---
+++ counter-app/src/Components/Counter.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    const onIncrementClicked = () => {  
+        setCount(count + 1);
+    };
+
+    const onDecrementClicked = () => {  
+        setCount(count - 1);
+    };
+
+    const onResetClicked = () => {  
+        setCount(0);
+    };
+
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <div>
+        <button onClick={onDecrementClicked}>-</button>
+        <button onClick={onResetClicked}>Reset</button>
+        <button onClick={onIncrementClicked}>+</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter;
